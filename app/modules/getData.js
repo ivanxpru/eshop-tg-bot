0d92471b122029728156e53bf8540e37ccd9a1bc
ivanxpru (lenovo) <8034400+ivanxpru@users.noreply.github.com>
2020-06-04 21:13:37 +0500

    add methods

--- app/modules/getData.js
+++ app/modules/getData.js
@@ -1,23 +1,52 @@
 const https = require('https');
 
-const getData = (url) => new Promise((resolve, reject) => {
-  https.get(url, (res) => {
-    let data = '';
-    if (res.statusCode === 200) {
-      res.on('data', (chunk) => { data += chunk; });
-      res.on('end', () => {
-        try {
-          resolve(JSON.parse(data, 'utf8'));
-        } catch (e) {
-          reject(e.message, data);
+exports.json = (url) =>
+  new Promise((resolve, reject) => {
+    https
+      .get(url, (res) => {
+        let data = '';
+        if (res.statusCode === 200) {
+          res.on('data', (chunk) => {
+            data += chunk;
+          });
+          res.on('end', () => {
+            try {
+              resolve(JSON.parse(data, 'utf8'));
+            } catch (e) {
+              reject(e.message, data);
+            }
+          });
+        } else {
+          reject(new Error(`getData ${url} ${res.statusCode}`));
         }
+      })
+      .on('error', (_e) => {
+        reject();
       });
-    } else {
-      reject(new Error(`getData ${url} ${res.statusCode}`));
-    }
-  }).on('error', (_e) => {
-    reject();
   });
-});
 
-module.exports = getData;
+exports.jsonp = (url, cb) =>
+  new Promise((resolve, reject) => {
+    https
+      .get(url, (res) => {
+        let data = '';
+        if (res.statusCode === 200) {
+          res.on('data', (chunk) => {
+            data += chunk;
+          });
+          res.on('end', () => {
+            try {
+              data = data.slice(cb.length + 1, data.length - 2);
+              resolve(JSON.parse(data, 'utf8'));
+            } catch (e) {
+              reject(e.message, data);
+            }
+          });
+        } else {
+          reject(new Error(`getData ${url} ${res.statusCode}`));
+        }
+      })
+      .on('error', (_e) => {
+        reject();
+      });
+  });
