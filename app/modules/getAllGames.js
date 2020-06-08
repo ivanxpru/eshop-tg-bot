9e1aad98595bb6cd020d230beafc45994381360b
ivanxpru (lenovo) <8034400+ivanxpru@users.noreply.github.com>
2020-06-09 00:14:54 +0500

    promisify for redis

--- app/modules/getAllGames.js
+++ app/modules/getAllGames.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-
+const { promisify } = require('util');
 const delay = require('delay');
 const redis = require('redis');
 const getData = require('./getData');
@@ -8,16 +8,28 @@ const doPost = require('./doPost');
 const logger = require('./logger');
 
 const redis_client = redis.createClient();
+const redis_client_set = promisify(redis_client.set).bind(redis_client);
+const redis_client_get = promisify(redis_client.get).bind(redis_client);
 
 const getAllgames = async (discount_b) => {
-  redis_client.set('getAllGames', 'true');
+  await redis_client_set('getAllGames', 'true');
   const url =
     'https://searching.nintendo-europe.com/ru/select?q=*&rows=9999&start=0&fq=type:GAME,DLC&sort=date_from%20asc&wt=json';
-  let games = await getData.json(url);
-  games = games.response.docs;
-  games = games.filter((docs) => docs.nsuid_txt);
+  let data = await getData.json(url);
+  data = data.response.docs;
+  data = data.filter((docs) => docs.nsuid_txt);
   if (discount_b) {
-    games = games.filter((docs) => docs.price_has_discount_b);
+    data = data.filter((docs) => docs.price_has_discount_b);
+  }
+  const games = [];
+  for await (const doc of data) {
+    if (doc.nsuid_txt[0]) {
+      await redis_client_get(doc.nsuid_txt[0]).then((reply) => {
+        if (!reply || (discount_b && reply - Date.now() <= 0)) {
+          games.push(doc);
+        }
+      });
+    }
   }
   for await (const game of games) {
     let post;
@@ -29,6 +41,11 @@ const getAllgames = async (discount_b) => {
     if (post) {
       await doPost(post, discount_b)
         .then(async () => {
+          if (discount_b) {
+            await redis_client_set(post.nsuid[0], post.discount_end_date);
+          } else {
+            await redis_client_set(post.nsuid[0], 0);
+          }
           await delay(1000 * 60 * 5);
         })
         .catch(() => {
--- app/modules/getGame.js
+++ app/modules/getGame.js
@@ -1,7 +1,4 @@
-const redis = require('redis');
 const getPrices = require('./getPrices');
-
-const redis_client = redis.createClient();
 
 const botname = process.env.BOT_USERNAME;
 
@@ -27,121 +24,117 @@ const system_types = [
 
 const getGame = (data, discount_b) =>
   new Promise((resolve, reject) => {
-    redis_client.get(data.nsuid_txt[0], async (_err, reply) => {
-      if (!reply || (discount_b && reply - Date.now() <= 0)) {
-        const post = {};
-        let prices;
-        let physical_version = false;
-        let nx = false;
-        let system = '';
-        let hashtags = '';
-        let image;
-        const keyboard = [];
-        if (Number.isInteger(Number.parseInt(data.nsuid_txt[0], 10))) {
-          post.nsuid = data.nsuid_txt;
-          if (data.wishlist_email_banner460w_image_url_s) {
-            image = `https:${data.wishlist_email_banner460w_image_url_s}`;
-          } else if (data.image_url) {
-            image = `https:${data.image_url}`;
-          } else {
-            image =
-              'https://upload.wikimedia.org/wikipedia/commons/9/95/Nintendo_Logo_2017.png';
-          }
-          image = image.replace(/_/g, '_');
-          if (data.system_type) {
-            data.system_type.forEach((platform) => {
-              if (system_types.includes(platform)) {
-                physical_version = true;
-              }
-            });
-          }
-          if (data.system_names_txt) {
-            data.system_names_txt.forEach((platform) => {
-              if (platform === 'Nintendo Switch') {
-                nx = true;
-              }
-              system += `#${platform.replace(/ /g, '_')} `;
-            });
-          } else if (data.required_system_txt) {
-            data.required_system_txt.forEach((platform) => {
-              system += `#${platform.replace(/ /g, '_')} `;
-            });
-          }
-          let title = `[${data.title}](https://nintendo.ru${data.url})\n${system}`;
-          title = title.replace(/_/g, '\\_');
-          let description = data.excerpt;
-          description = description.replace(/^DLC/g, '#DLC');
-          if (data.game_categories_txt) {
-            data.game_categories_txt.forEach((category) => {
-              hashtags += `#${category} `;
-            });
-            hashtags += '\n';
-          }
-          if (data.language_availability) {
-            data.language_availability[0].split(',').forEach((language) => {
-              hashtags += `#${language} `;
-            });
-          }
-          hashtags = hashtags.replace(/_/g, '\\_');
-          await getPrices(data.nsuid_txt[0], discount_b)
-            .then((res) => {
-              prices = res;
-            })
-            .catch(() => {});
-          if (!prices) {
-            return reject(new Error('no get prices'));
-          }
-          if (data.physical_version_b || physical_version) {
-            const buttons = [];
-            let button = {};
-            button.text = 'Купить б/у';
-            button.url = `https://t.me/${botname}?start=buy_${data.nsuid_txt[0]}`;
-            buttons.push(button);
-            button = {};
-            button.text = 'Продать б/у';
-            button.url = `https://t.me/${botname}?start=sell_${data.nsuid_txt[0]}`;
-            buttons.push(button);
-            keyboard.push(buttons);
-          }
-          /*
-          if (data.near_field_comm_b) {
-            const buttons = [];
-            const button = {};
-            button.text = 'Совместимые amiibo';
-            button.url = `https://t.me/${botname}?start=amiibo_${data.fs_id}`;
-            buttons.push(button);
-            keyboard.push(buttons);
-          }
-          */
-          if ((data.cloud_saves_b && nx) || (data.internet && nx)) {
-            const buttons = [];
-            const button = {};
-            button.text = 'Nintendo Switch Online (350 р./год)';
-            button.url = `https://t.me/${botname}?start=family_subscribe`;
-            buttons.push(button);
-            keyboard.push(buttons);
-          }
-          if (prices) {
-            post.message = `${title}\n\n${description}\n${hashtags}\n\n${prices.prices}`;
-          } else {
-            post.message = `${title}\n\n${description}\n${hashtags}\n\nSOLD OUT`;
-          }
-          post.image = image;
-          if (keyboard) {
-            post.keyboard = keyboard;
-          }
-          if (discount_b && prices.discount_end_date) {
-            post.discount_end_date = prices.discount_end_date;
-          }
-          resolve(post);
+    (async () => {
+      const post = {};
+      let prices;
+      let physical_version = false;
+      let nx = false;
+      let system = '';
+      let hashtags = '';
+      let image;
+      const keyboard = [];
+      if (Number.isInteger(Number.parseInt(data.nsuid_txt[0], 10))) {
+        post.nsuid = data.nsuid_txt;
+        if (data.wishlist_email_banner460w_image_url_s) {
+          image = `https:${data.wishlist_email_banner460w_image_url_s}`;
+        } else if (data.image_url) {
+          image = `https:${data.image_url}`;
         } else {
-          console.log(data.title, data.nsuid_txt[0]);
-          reject(new Error(`Not valid nsuid: ${data.nsuid_txt[0]}`));
+          image =
+            'https://upload.wikimedia.org/wikipedia/commons/9/95/Nintendo_Logo_2017.png';
         }
+        image = image.replace(/_/g, '_');
+        if (data.system_type) {
+          data.system_type.forEach((platform) => {
+            if (system_types.includes(platform)) {
+              physical_version = true;
+            }
+          });
+        }
+        if (data.system_names_txt) {
+          data.system_names_txt.forEach((platform) => {
+            if (platform === 'Nintendo Switch') {
+              nx = true;
+            }
+            system += `#${platform.replace(/ /g, '_')} `;
+          });
+        } else if (data.required_system_txt) {
+          data.required_system_txt.forEach((platform) => {
+            system += `#${platform.replace(/ /g, '_')} `;
+          });
+        }
+        let title = `[${data.title}](https://nintendo.ru${data.url})\n${system}`;
+        title = title.replace(/_/g, '\\_');
+        let description = data.excerpt;
+        description = description.replace(/^DLC/g, '#DLC');
+        if (data.game_categories_txt) {
+          data.game_categories_txt.forEach((category) => {
+            hashtags += `#${category} `;
+          });
+          hashtags += '\n';
+        }
+        if (data.language_availability) {
+          data.language_availability[0].split(',').forEach((language) => {
+            hashtags += `#${language} `;
+          });
+        }
+        hashtags = hashtags.replace(/_/g, '\\_');
+        await getPrices(data.nsuid_txt[0], discount_b)
+          .then((res) => {
+            prices = res;
+          })
+          .catch(() => {});
+        if (!prices) {
+          return reject(new Error('no get prices'));
+        }
+        if (data.physical_version_b || physical_version) {
+          const buttons = [];
+          let button = {};
+          button.text = 'Купить б/у';
+          button.url = `https://t.me/${botname}?start=buy_${data.nsuid_txt[0]}`;
+          buttons.push(button);
+          button = {};
+          button.text = 'Продать б/у';
+          button.url = `https://t.me/${botname}?start=sell_${data.nsuid_txt[0]}`;
+          buttons.push(button);
+          keyboard.push(buttons);
+        }
+        /*
+        if (data.near_field_comm_b) {
+          const buttons = [];
+          const button = {};
+          button.text = 'Совместимые amiibo';
+          button.url = `https://t.me/${botname}?start=amiibo_${data.fs_id}`;
+          buttons.push(button);
+          keyboard.push(buttons);
+        }
+        */
+        if ((data.cloud_saves_b && nx) || (data.internet && nx)) {
+          const buttons = [];
+          const button = {};
+          button.text = 'Nintendo Switch Online (350 р./год)';
+          button.url = `https://t.me/${botname}?start=family_subscribe`;
+          buttons.push(button);
+          keyboard.push(buttons);
+        }
+        if (prices) {
+          post.message = `${title}\n\n${description}\n${hashtags}\n\n${prices.prices}`;
+        } else {
+          post.message = `${title}\n\n${description}\n${hashtags}\n\nSOLD OUT`;
+        }
+        post.image = image;
+        if (keyboard) {
+          post.keyboard = keyboard;
+        }
+        if (discount_b && prices.discount_end_date) {
+          post.discount_end_date = prices.discount_end_date;
+        }
+        resolve(post);
       } else {
-        reject(new Error('Game has posted before'));
+        console.log(data.title, data.nsuid_txt[0]);
+        reject(new Error(`Not valid nsuid: ${data.nsuid_txt[0]}`));
       }
-    });
+    })();
   });
 
 module.exports = getGame;
