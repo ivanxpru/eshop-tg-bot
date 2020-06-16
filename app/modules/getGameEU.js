8fdcfcb03d86935d4a9bc6df3f202d704c47f6d1
ivanxpru (lenovo) <8034400+ivanxpru@users.noreply.github.com>
2020-06-17 00:11:31 +0500

    getGamesEU ready

---
+++ app/modules/getGameEU.js
@@ -0,0 +1,187 @@
+const getPrices = require('./getPrices');
+const logger = require('./logger');
+
+const botname = process.env.BOT_USERNAME;
+
+const system_types = [
+  'nes_cartridge',
+  'snes_cartridge',
+  'nintendo64_cartridge',
+  'gamecube_disc',
+  'wii_disc',
+  'wiiu_disc',
+  'nds_cartridge',
+  '3ds_cartridge',
+  'newnintendo3ds_gamecard',
+  'nintendoswitch_gamecard',
+  'commodore64',
+  'gameboy_cartridge',
+  'gameboycolor_cartridge',
+  'gameboyadvance_cartridge',
+  'sega_gamegear',
+  'sega_megadrive',
+  'turbografx',
+];
+
+const getData = (data) => {
+  const game = {};
+  game.fs_id = Number.parseInt(data.fs_id, 10);
+  game.title_eu = data.title;
+  game.categories = [];
+  if (data.game_categories_txt) {
+    for (const category of data.game_categories_txt) {
+      game.categories.push(category);
+    }
+  }
+  game.system_type = [];
+  if (data.system_type) {
+    for (const system of data.system_type) {
+      system.split(',').forEach((item) => {
+        if (item) {
+          if (system_types.includes(item)) {
+            game.physical_version = true;
+          }
+          game.system_type.push(item);
+        } else {
+          if (system_types.includes(system)) {
+            game.physical_version = true;
+          } else {
+            game.physical_version = !!data.physical_version_b;
+          }
+          game.system_type.push(system);
+        }
+      });
+    }
+  }
+  game.system_name = [];
+  if (data.system_names_txt) {
+    for (const system_name of data.system_names_txt) {
+      game.system_name.push(system_name);
+    }
+  } else if (data.required_system_txt) {
+    for (const system_name of data.required_system_txt) {
+      game.system_name.push(system_name);
+    }
+  }
+  game.publisher_eu = data.publisher;
+  game.date_release_eu = new Date(Date.parse(data.date_from));
+  game.description_eu = data.excerpt;
+  game.url_eu = data.url;
+  game.languages = [];
+  if (data.language_availability) {
+    data.language_availability[0].split(',').forEach((language) => {
+      game.languages.push(language);
+    });
+  }
+  game.nsuid_eu = Number.parseInt(data.nsuid_txt[0], 10);
+  game.mode_tv = !!data.play_mode_tv_mode_b;
+  game.mode_handheld = !!data.play_mode_handheld_mode_b;
+  game.mode_tabletop = !!data.play_mode_tabletop_mode_b;
+  game.hd_rumble = !!data.hd_rumble_b;
+  game.ir_camera = !!data.ir_motion_camera_b;
+  game.amiibo = !!data.near_field_comm_b;
+  game.labo = !!data.labo_b;
+  game.cloud_saves = !!data.cloud_saves_b;
+  game.internet = !!data.internet;
+  game.local_play = !!data.local_play;
+  game.voice_chat = !!data.voice_chat_b;
+  game.fts = Math.ceil(data.price_sorting_f) === 0;
+  game.demo = !!data.demo_availability;
+  game.dlc = !!data.dlc_shown_b;
+  game.subscription = !!data.paid_subscription_required_b;
+  if (data.wishlist_email_banner460w_image_url_s) {
+    game.boxart_eu = `https:${data.wishlist_email_banner460w_image_url_s}`;
+  } else if (data.image_url) {
+    game.boxart_eu = `https:${data.image_url}`;
+  } else {
+    game.boxart_eu =
+      'https://upload.wikimedia.org/wikipedia/commons/9/95/Nintendo_Logo_2017.png';
+  }
+  return game;
+};
+
+const getGame = (data, discount_b) =>
+  new Promise((resolve, _reject) => {
+    (async () => {
+      const response = [];
+      const game = getData(data);
+      response.game = game;
+      const post = {};
+      let prices;
+      let nx = false;
+      let system = '';
+      let hashtags = '';
+      let image = game.boxart_eu;
+      image = image.replace(/_/g, '_');
+      const keyboard = [];
+      for (const platform of game.system_name) {
+        if (platform === 'Nintendo Switch') {
+          nx = true;
+        }
+        system += `#${platform.replace(/ /g, '_')} `;
+      }
+      system = system.replace(/_/g, '\\_');
+      const title = `[${game.title_eu}](https://nintendo.ru${game.url_eu})\n${system}`;
+      let description = game.description_eu;
+      description = description.replace(/^DLC/g, '#DLC');
+      for (const category of game.categories) {
+        hashtags += `#${category} `;
+      }
+      for (const language of game.languages) {
+        hashtags += `#${language} `;
+      }
+      if (game.amiibo) {
+        hashtags += `#amiibo`;
+      }
+      hashtags = hashtags.replace(/_/g, '\\_');
+      if (game.nsuid_eu) {
+        await getPrices
+          .EU(game.nsuid_eu, discount_b)
+          .then((res) => {
+            prices = res;
+          })
+          .catch((err) => {
+            logger.log('error', err);
+          });
+      }
+      if (!prices) {
+        prices = false;
+      }
+      if (game.physical_version) {
+        const buttons = [];
+        let button = {};
+        button.text = 'Купить б/у';
+        button.url = `https://t.me/${botname}?start=buyEU_${data.fs_id}`;
+        buttons.push(button);
+        button = {};
+        button.text = 'Продать б/у';
+        button.url = `https://t.me/${botname}?start=sellEU_${data.fs_id}`;
+        buttons.push(button);
+        keyboard.push(buttons);
+      }
+      if ((game.cloud_saves && nx) || (game.internet && nx)) {
+        const buttons = [];
+        const button = {};
+        button.text = 'Nintendo Switch Online (350 р./год)';
+        button.url = `https://t.me/${botname}?start=family_subscribe`;
+        buttons.push(button);
+        keyboard.push(buttons);
+      }
+      if (prices) {
+        post.message = `${title}\n\n${description}\n${hashtags}\n\n${prices.prices}`;
+      } else {
+        post.message = `${title}\n\n${description}\n${hashtags}\n\nSOLD OUT`;
+      }
+      post.image = image;
+      if (keyboard) {
+        post.keyboard = keyboard;
+      }
+      if (discount_b && prices.discount_end_date) {
+        post.discount_end_date = prices.discount_end_date;
+      }
+      response.post = post;
+      return resolve(response);
+    })();
+  });
+
+module.exports = getGame;
---
+++ app/modules/getGamesEU.js
@@ -0,0 +1,95 @@
+require('dotenv').config();
+const SocksAgent = require('socks5-https-client/lib/Agent');
+const Telegram = require('telegraf/telegram');
+const { promisify } = require('util');
+const delay = require('delay');
+const redis = require('redis');
+const getData = require('./getData');
+const getDB = require('./getDB');
+const getGameEU = require('./getGameEU');
+const logger = require('./logger');
+
+let telegram;
+if (process.env.SOCKS_HOST && process.env.SOCKS_PORT) {
+  const socksAgent = new SocksAgent({
+    socksHost: process.env.SOCKS_HOST,
+    socksPort: process.env.SOCKS_PORT,
+  });
+  telegram = new Telegram(process.env.BOT_TOKEN, {
+    agent: socksAgent,
+  });
+} else {
+  telegram = new Telegram(process.env.BOT_TOKEN, {});
+}
+
+const channel = process.env.CHANNEL_DEBUG;
+const url_games_eu = process.env.URL_GAMES_EU;
+const games_eu = process.env.GAMES_EU;
+
+const redis_client = redis.createClient();
+const redis_client_set = promisify(redis_client.set).bind(redis_client);
+const redis_client_get = promisify(redis_client.get).bind(redis_client);
+
+const getGamesEU = async (discount_b) => {
+  await redis_client_set('getAllGames', 'true');
+  let data = await getData.json(url_games_eu);
+  data = data.response.docs;
+  data = data.filter((docs) => docs.nsuid_txt);
+  if (discount_b) {
+    data = data.filter((docs) => docs.price_has_discount_b);
+  }
+  for await (const doc of data) {
+    if (!doc.date_from) {
+      data.push(data.shift());
+    }
+  }
+  const games = [];
+  for await (const doc of data) {
+    await redis_client_get(doc.fs_id).then((reply) => {
+      if (!reply || (discount_b && reply - Date.now() <= 0)) {
+        games.push(doc);
+      }
+    });
+  }
+  for await (const game of games) {
+    let result;
+    await getGameEU(game, discount_b)
+      .then((res) => {
+        result = res;
+      })
+      .catch((err) => {
+        logger.log('error', err);
+      });
+    if (result) {
+      const options = {
+        caption: result.post.message,
+        parse_mode: 'Markdown',
+        reply_markup: JSON.stringify({
+          inline_keyboard: result.post.keyboard,
+        }),
+      };
+      await telegram
+        .sendPhoto(channel, result.post.image, options)
+        .then(async () => {
+          if (discount_b) {
+            await redis_client_set(
+              result.game.fs_id,
+              result.post.discount_end_date,
+            );
+          } else {
+            await redis_client_set(result.game.fs_id, 0);
+          }
+          await getDB.addEU(result.game, games_eu).catch((err) => {
+            logger.log('error', err);
+          });
+          await delay(1000 * 60 * 2);
+        })
+        .catch((err) => {
+          logger.log('error', `sendPhoto: ${err}`);
+        });
+    }
+  }
+  redis_client.del('getAllGames');
+};
+
+module.exports = getGamesEU;
