9d28dd653b9530472e5556d4cc36538c2f6f5ae8
ivanxpru (lenovo) <8034400+ivanxpru@users.noreply.github.com>
2020-06-25 15:00:04 +0500

    stage moved to stage.js

--- app/modules/bots/mainBot.js
+++ app/modules/bots/mainBot.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const delay = require('delay');
 const path = require('path');
 const Telegraf = require('telegraf');
-const Stage = require('telegraf/stage');
 const Composer = require('telegraf/composer');
 const TelegrafI18n = require('telegraf-i18n');
-const session = require('telegraf/session');
+// const session = require('telegraf/session');
 const RedisSession = require('telegraf-session-redis');
+const stage = require('../scenes/stage');
 const logger = require('../logger');
 const adminBot = require('./adminBot');
 const regularBot = require('./regularBot');
@@ -26,7 +26,7 @@ const i18n = new TelegrafI18n({
   },
 });
 
-const session1 = new RedisSession({
+const session = new RedisSession({
   store: {
     host: process.env.TELEGRAM_SESSION_HOST || '127.0.0.1',
     port: process.env.TELEGRAM_SESSION_PORT || 6379,
@@ -35,62 +35,7 @@ const session1 = new RedisSession({
 
 const adminIds = [246542665, 2205222443];
 
-// Сценарии
-const familyScene = require('../scenes/familyScene');
-const [buyScene, buySceneSt1, buySceneSt2] = require('../scenes/buyScene');
-const [
-  sellScene,
-  sellSceneSt1,
-  sellSceneSt2,
-  sellSceneSt3,
-  sellSceneSt4,
-  sellSceneSt5,
-] = require('../scenes/sellScene');
-const [
-  lotsScene,
-  lotsSceneSt1,
-  lotsSceneSt2,
-  lotsSceneSt3,
-  lotsSceneSt4,
-  lotsSceneSt5,
-  lotsSceneSt6,
-] = require('../scenes/lotsScene');
-const verifyScene = require('../scenes/verifyScene');
-
-const stage = new Stage();
-
-stage.action('cancel', async (ctx) => {
-  await ctx.deleteMessage(ctx.update.callback_query.message.message_id);
-  ctx.session = {};
-  ctx.scene.leave();
-});
-
-stage.start(async (ctx, next) => {
-  ctx.session = {};
-  ctx.scene.leave();
-  return next();
-});
-
-stage.register(familyScene);
-stage.register(buyScene);
-stage.register(buySceneSt1);
-stage.register(buySceneSt2);
-stage.register(sellScene);
-stage.register(sellSceneSt1);
-stage.register(sellSceneSt2);
-stage.register(sellSceneSt3);
-stage.register(sellSceneSt4);
-stage.register(sellSceneSt5);
-stage.register(lotsScene);
-stage.register(lotsSceneSt1);
-stage.register(lotsSceneSt2);
-stage.register(lotsSceneSt3);
-stage.register(lotsSceneSt4);
-stage.register(lotsSceneSt5);
-stage.register(lotsSceneSt6);
-stage.register(verifyScene);
-
-bot.use(session());
+bot.use(session);
 bot.use(i18n.middleware());
 bot.use(stage.middleware());
 bot.use(Composer.acl(adminIds, adminBot));
@@ -105,23 +50,11 @@ bot.use(regularBot);
 
 bot.command('ping', async (ctx) => {
   await ctx.deleteMessage(ctx.message.message_id);
-  await ctx.reply('pong').then((ctx_reply) => {
-    setTimeout(async () => {
-      await ctx.deleteMessage(ctx_reply.message_id);
-    }, 5000);
-  });
+  await ctx.reply('pong');
 });
 
 bot.on('callback_query', async (ctx, next) => {
   if (!ctx.session.__scenes.current) {
-    // удаляет сообщения вне сцен
-    await ctx
-      .answerCbQuery(ctx.i18n.t('Сообщение устарело'), true)
-      .catch(() => {});
-    await ctx.telegram
-      .deleteMessage(ctx.chat.id, ctx.update.callback_query.message.message_id)
-      .catch(() => {});
-  } else {
     const data = ctx.update.callback_query.data;
     ctx.update.callback_query.message.reply_markup.inline_keyboard.forEach(
       async (keyboard) => {
@@ -142,57 +75,54 @@ bot.start(async (ctx, next) => {
   await getUser(ctx.from.id)
     .then((res) => {
       ctx.state.user = res;
+      next();
     })
-    .catch((err) => {
-      console.error(err);
-    });
+    .catch(() => {});
   switch (ctx.startPayload.split('_')[0]) {
     case 'family':
       ctx.scene.enter('family_scene');
       break;
     case 'sellEU':
+      if (!ctx.state.user.verify) {
+        ctx.scene.enter('verify_scene');
+        break;
+      }
       await getData
         .json(
-          `https://eshopdb.ivanxpru.repl.co/api/v1.0/games/full/?query=${
+          `https://eshopdb.ivanxpru.repl.co/api/v1.0/games/eu/?query=${
             ctx.startPayload.split('_')[1]
           }&field=fs_id`,
         )
         .then((res) => {
           ctx.state.game = res.games[0];
           ctx.state.game.title = res.games[0].title_eu;
+          ctx.scene.enter('sell_scene');
         })
         .catch((err) => {
+          ctx.reply(ctx.i18n.t('sell_scene_txt3'));
           logger.log('error', err);
         });
-      if (ctx.state.user.verify) {
-        next();
-        ctx.scene.enter('sell_scene');
-      } else {
-        next();
-        ctx.scene.enter('verify_scene');
-      }
       break;
     case 'buyEU':
+      if (!ctx.state.user.verify) {
+        ctx.scene.enter('verify_scene');
+        break;
+      }
       await getData
         .json(
-          `https://eshopdb.ivanxpru.repl.co/api/v1.0/games/full/?query=${
+          `https://eshopdb.ivanxpru.repl.co/api/v1.0/games/eu/?query=${
             ctx.startPayload.split('_')[1]
           }&field=fs_id`,
         )
         .then((res) => {
           ctx.state.game = res.games[0];
           ctx.state.game.title = res.games[0].title_eu;
+          ctx.scene.enter('buy_scene');
         })
         .catch((err) => {
+          ctx.reply(ctx.i18n.t('buy_scene_txt3'));
           logger.log('error', err);
         });
-      if (ctx.state.user.verify) {
-        next();
-        ctx.scene.enter('buy_scene');
-      } else {
-        next();
-        ctx.scene.enter('verify_scene');
-      }
       break;
     case 'view':
       ctx.session.state.lot._id = ctx.update.callback_query.data.split('_')[1];
