33f78ee132b4b5005d3625cc43bbe7bfbc6260c1
ivanxpru (lenovo) <8034400+ivanxpru@users.noreply.github.com>
2020-06-25 15:01:58 +0500

    update via prettier/eslint

--- app/modules/getLot.js
+++ app/modules/getLot.js
@@ -6,86 +6,94 @@ const dbURI = process.env.DB_URI;
 const dbName = process.env.DB_NAME;
 const dbCollection = 'lots';
 
-exports.add = (data) => new Promise((resolve, reject) => {
-  db.connect(dbURI, dbName, async (err) => {
-    if (err) return console.error(err);
-    const collection = db.get().collection(dbCollection);
-    await collection.insertOne(data)
-      .then((res) => {
-        resolve(res.insertedId);
-      })
-      .catch(() => { reject(new Error(data)); });
-  });
-});
-
-exports.getByLotId = (lot_id) => new Promise((resolve, reject) => {
-  db.connect(dbURI, dbName, async (err) => {
-    if (err) reject(err);
-    const collection = db.get().collection(dbCollection);
-    const details = { _id: ObjectId(lot_id) };
-    await collection.findOne(details, (error, result) => {
-      if (error) {
-        console.log(error);
-        reject(err);
-      }
-      resolve(result);
+exports.add = (data) =>
+  new Promise((resolve, reject) => {
+    db.connect(dbURI, dbName, async (err) => {
+      if (err) return console.error(err);
+      const collection = db.get().collection(dbCollection);
+      await collection
+        .insertOne(data)
+        .then((res) => {
+          resolve(res.insertedId);
+        })
+        .catch(() => {
+          reject(new Error(data));
+        });
     });
   });
-});
 
-exports.getByUserId = (user_id, status) => new Promise((resolve, reject) => {
-  db.connect(dbURI, dbName, async (err) => {
-    if (err) return console.error(err);
-    let details;
-    const collection = db.get().collection(dbCollection);
-    if (!status) {
-      details = { user_id };
-    } else {
-      details = { user_id, status };
-    }
-    await collection.find(details).toArray((error, result) => {
-      if (err) {
-        console.log(error);
-        reject();
-      }
-      if (result.length) {
+exports.getByLotId = (lot_id) =>
+  new Promise((resolve, reject) => {
+    db.connect(dbURI, dbName, async (err) => {
+      if (err) reject(err);
+      const collection = db.get().collection(dbCollection);
+      const details = { _id: ObjectId(lot_id) };
+      await collection.findOne(details, (error, result) => {
+        if (error) {
+          console.log(error);
+          reject(err);
+        }
         resolve(result);
+      });
+    });
+  });
+
+exports.getByUserId = (user_id, status) =>
+  new Promise((resolve, reject) => {
+    db.connect(dbURI, dbName, async (err) => {
+      if (err) return console.error(err);
+      let details;
+      const collection = db.get().collection(dbCollection);
+      if (!status) {
+        details = { user_id };
       } else {
-        reject();
+        details = { user_id, status };
+      }
+      await collection.find(details).toArray((error, result) => {
+        if (err) {
+          console.log(error);
+          reject();
+        }
+        if (result.length) {
+          resolve(result);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+
+exports.getByFsId = (fs_id) =>
+  new Promise((resolve, reject) => {
+    db.connect(dbURI, dbName, async (err) => {
+      if (err) return console.error(err);
+      const collection = db.get().collection(dbCollection);
+      const details = { fs_id };
+      await collection.find(details).toArray((error, result) => {
+        if (error) {
+          console.log(err);
+          reject();
+        }
+        if (result.length) {
+          resolve(result);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+
+exports.changeStatus = (lot_id, status) =>
+  new Promise((resolve, reject) => {
+    db.connect(dbURI, dbName, async (err) => {
+      if (err) reject(err);
+      const collection = db.get().collection(dbCollection);
+      const details = { _id: ObjectId(lot_id) };
+      try {
+        await collection.updateOne(details, { $set: { status } });
+        resolve(true);
+      } catch {
+        reject(new Error('changeStatus'));
       }
     });
   });
-});
-
-exports.getByNsuid = (nsuid) => new Promise((resolve, reject) => {
-  db.connect(dbURI, dbName, async (err) => {
-    if (err) return console.error(err);
-    const collection = db.get().collection(dbCollection);
-    const details = { nsuid };
-    await collection.find(details).toArray((error, result) => {
-      if (error) {
-        console.log(err);
-        reject();
-      }
-      if (result.length) {
-        resolve(result);
-      } else {
-        reject();
-      }
-    });
-  });
-});
-
-exports.changeStatus = (lot_id, status) => new Promise((resolve, reject) => {
-  db.connect(dbURI, dbName, async (err) => {
-    if (err) reject(err);
-    const collection = db.get().collection(dbCollection);
-    const details = { _id: ObjectId(lot_id) };
-    try {
-      await collection.updateOne(details, { $set: { status } });
-      resolve(true);
-    } catch {
-      reject(new Error('changeStatus'));
-    }
-  });
-});
